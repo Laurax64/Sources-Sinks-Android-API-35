4a0282e2daa6b0b4a742ca69fb51641ef8d13dea
Laura Voetzsch <lauravoetzsch@gmail.com>
2025-01-29 14:54:51 +0100

    Add implemented methods for Notification.TvExtender

--- js/methods-extractor.js
+++ js/methods-extractor.js
@@ -167,8 +167,7 @@ function getDataReturned(returnType, imports, packageName, changeName, classOrIn
             description = `0 which indicates, that the contents are not meant to cross compilation boundaries`
         }
         else if (!commonSensitiveTypes.includes(returnType)) {
-            description = `An object of type ${returnType} that does not contain sensitive data that can be accessed 
-            without calling another function`
+            description = `An object of type ${returnType} that does not contain sensitive data that can be accessed without calling another function`
         }
 
         dataReturned.push({
